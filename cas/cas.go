338ca95b0f8c8c344ad2eefcbf4faeeccce51be9
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-05-30 15:19:16 +0300

    Initial commit

---
+++ cas/cas.go
@@ -0,0 +1,20 @@
+package cas
+
+import (
+	goCas "github.com/go-cas/cas"
+)
+
+type casMiddleware {
+	casClient *goCas.Client
+}
+
+func (casMiddleware casMiddleware) casMiddleware(c *gin.Context) {
+	casMiddleware.casClient.Handler(c.Writer, c.Request)
+	c.Set("CASUsername", casMiddleware.casClient.Username(c.Request))
+	c.Set("CASAttributes", casMiddleware.casClient.Attributes(c.Request))
+}
+
+func MiddlewareFunc(casClient *goCas.Client) gin.HandlerFunc {
+	return casMiddleware{casClient: casClient}.middlewareFunc
+}
+
