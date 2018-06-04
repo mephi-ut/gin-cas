2d8f9e7dd1cb455c458c9bc2ae740476be10fd54
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-06-04 19:07:26 +0300

    bugfixes

--- cas/cas.go
+++ cas/cas.go
@@ -1,19 +1,46 @@
 package cas
 
 import (
+	"github.com/gin-gonic/gin"
 	goCas "github.com/go-cas/cas"
+	"net/http"
 )
 
 type casMiddleware struct {
 	casClient *goCas.Client
+	handler http.Handler
 }
 
-func (casMiddleware casMiddleware) casMiddleware(c *gin.Context) {
-	casMiddleware.casClient.Handler(c.Writer, c.Request)
-	c.Set("CASUsername", casMiddleware.casClient.Username(c.Request))
-	c.Set("CASAttributes", casMiddleware.casClient.Attributes(c.Request))
+func (casMiddleware casMiddleware) authed(c *gin.Context) {
+	c.Set("CASUsername", goCas.Username(c.Request))
+	c.Set("CASAttributes", goCas.Attributes(c.Request))
+	c.Next()
+	return
 }
 
-func MiddlewareFunc(casClient *goCas.Client) gin.HandlerFunc {
-	return casMiddleware{casClient: casClient}.middlewareFunc
+func (casMiddleware casMiddleware) middlewareFunc(c *gin.Context) {
+	casMiddleware.handler.ServeHTTP(c.Writer, c.Request)
+	if goCas.IsAuthenticated(c.Request) {
+		casMiddleware.authed(c)
+		return
+	}
+	c.Abort()
+}
+
+type Options = goCas.Options
+func MiddlewareFunc(options *Options) gin.HandlerFunc {
+	casClient := goCas.NewClient((*goCas.Options)(options))
+	rawHandler := func(res http.ResponseWriter, req *http.Request) {
+		if goCas.IsAuthenticated(req) {
+			return
+		}
+		if goCas.IsNewLogin(req) {
+			return
+		}
+		casClient.RedirectToLogin(res, req)
+	}
+	return casMiddleware{
+		casClient: casClient,
+		handler: casClient.HandleFunc(rawHandler),
+	}.middlewareFunc
 }
